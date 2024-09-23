5c37181a26557b1efc4c05cd604083d1ba41476b
adaiuto1 <78216021+adaiuto1@users.noreply.github.com>
2024-09-23 15:55:58 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,11 @@ const corsOption = {
     origin: ["*"],
 };
 app.use((0, cors_1.default)(corsOption));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
