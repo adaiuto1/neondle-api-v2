dc270879361d2e844a3ce5c53e64e7ddd2a6a24e
adaiuto1 <78216021+adaiuto1@users.noreply.github.com>
2024-09-23 15:38:00 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import { levelRouter } from "./routers/levelRouter";
 import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(cors());
 const PORT = 8000;
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const cors = require('cors');
+
+const corsOption = {
+    credentials: true,
+    origin: ['http://localhost:3000', 'http://localhost:80']
+}
+
+app.use(cors(corsOption));
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
