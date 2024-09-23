864580392016f2f9b6744c715bc3e67f6648f8fb
adaiuto1 <78216021+adaiuto1@users.noreply.github.com>
2024-09-23 15:55:35 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,18 @@ const PORT = 8000;
 
 const corsOption = {
 	credentials: true,
-	origin: ["*"],
+	origin: ["*", "*"],
 };
 
 app.use(cors(corsOption));
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
