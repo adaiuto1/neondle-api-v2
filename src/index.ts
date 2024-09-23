6fa8c8d48c1cf4a7749a3e114f5ba951ecff17e0
adaiuto1 <78216021+adaiuto1@users.noreply.github.com>
2024-09-23 15:37:04 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = 8000;
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
